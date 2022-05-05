515b614a9239035c93ad08172466d107b77a3b31
Markus D <markusdrx@gmail.com>
2022-05-05 20:52:36 +0200

    added readme - changed CourseInput practicing dynamic css

---
+++ README.md
@@ -0,0 +1,1 @@
+# Second Project - Styling practice - Goal Tracker
--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
+    // To check if the input is valid, lets the state to true - so it resets back to normal after start typing
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    // To validate if entered value is valid or not, to show different styling depending on that
+    // Validate if its empty, if not, it will add the entered value
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    } else props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/* Adding dynamic styling - depending on user input and isValid state */}
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
